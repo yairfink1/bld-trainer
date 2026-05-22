14e7af247aa31a93f31ef52369a3005528e9b99c
Yair Fink <noamfinkyair@gmail.com>
2026-05-22 12:11:26 +0300

    style: left-align scramble moves, pin to top of trainer, bump to v19

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker for BLD Trainer — enables offline PWA
-const CACHE_NAME = 'bld-trainer-v18';
+const CACHE_NAME = 'bld-trainer-v19';
 const ASSETS = [
     './',
     './index.html',
